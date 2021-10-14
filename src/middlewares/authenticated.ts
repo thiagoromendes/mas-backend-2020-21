e945b43aee0995fe07db0f910a1433420b04705d
Thiago Mendes <thiagoromendes@gmail.com>
2021-10-14 16:37:59 -0300

    chore: add new middleware for auth

---
+++ src/middlewares/authenticated.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import authConfig from "../config/auth";
+
+interface Token {
+    sub: string;
+    role: string;
+}
+
+export default function authenticated(request:Request, response:Response, next: NextFunction){
+
+    const headerAuthorization = request.headers.authorization
+
+    if(!headerAuthorization){
+        return response.json({
+            error: "Token not found"
+        })
+    }
+
+    const [, token] = headerAuthorization.split(' ');
+
+    const verifyToken = verify(token,authConfig.jwt.secret);
+
+    if(!verifyToken){
+        throw new Error();
+    }
+    
+    const {sub, role} = verifyToken as Token;
+
+    request.body.user = {
+        id: sub,
+        role: role
+    }
+
+    return next();
+
+}
